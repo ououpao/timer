ec376964a40434857c9a61f2f673fdce871e09de
kraaas <570453516@qq.com>
2016-09-27 14:21:08 +0800

    add log list

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -1,16 +1,38 @@
 var util = require('../../utils/util.js')
 Page({
   data: {
-    logs: []
+    logs: [],
+    modalHidden: true,
+    toastHidden: true
   },
   onShow: function() {
     wx.setNavigationBarTitle({
       title: '任务记录'
     })
+    this.getLogs()
   },
-  onLoad: function () {
+  getLogs: function() {
     this.setData({
       logs: wx.getStorageSync('logs')
     })
+  },
+  onLoad: function() {},
+  switchModal: function() {
+    this.setData({
+      modalHidden: !this.data.modalHidden
+    })
+  },
+  hideToast: function() {
+    this.setData({
+      toastHidden: true
+    })
+  },
+  clearLog: function(e) {
+    wx.setStorageSync('logs', [])
+    this.switchModal()
+    this.setData({
+      toastHidden: false
+    })
+    this.getLogs()
   }
 })
